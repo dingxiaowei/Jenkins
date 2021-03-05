4fdb58628a381cda33bebf28010003c306ed65a4
dingxiaowei <1213250243@qq.com>
2021-03-05 15:24:18 +0800

    可运行

--- HelloUnity/Assets/Unity.bat
+++ HelloUnity/Assets/Unity.bat
@@ -1,12 +1,12 @@
 @echo off
 echo update
-call test.bat
 
 echo "Delete Old Version"
-rd /s /Q D:\git\JenkinsTest\build
+rd /s /Q D:\study\Unity\Jenkins\build
 
 echo "Start build apk..."
-D:\Unity20170412\Unity\Editor\Unity.exe -projectPath D:\git\jenkinsTest\HelloUnity -quit -batchmode -executeMethod PerformBuild.CommandLineBuildAndroid -logFile build.log
+
+C:\"Program Files"\Unity\Editor\Unity.exe -projectPath D:\study\Unity\Jenkins\HelloUnity -quit -batchmode -executeMethod PerformBuild.CommandLineBuildAndroid -logFile build.log
 REM %1 -projectPath %2 -quit -batchmode -executeMethod APKBuild.Build -logFile build.log
 
 if not %errorlevel%==0 ( goto fail ) else ( goto success )
@@ -15,7 +15,7 @@ if not %errorlevel%==0 ( goto fail ) else ( goto success )
 echo Build APK OK
 
 echo Copy APK
-xcopy D:\git\JenkinsTest\build\android\HelloUnity.apk D:\ftpserver\ /y
+xcopy D:\study\Unity\Jenkins\build\android\HelloUnity.apk D:\back\ /y
 
 if not %errorlevel%==0 ( goto fail ) else ( goto end )
  
